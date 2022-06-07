095c2e6d7397a3bbae4e42f616202749d4feebc3
Maireno <gabrielmaireno@hotmail.com>
2022-06-07 10:24:46 -0300

    Logout nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { routes } from './app.routing';
-import { RegistrarComponent } from './registrar/registrar.component';
 import { NavBarCliComponent } from './nav-bar-cli/nav-bar-cli.component';
 import { FooterComponent } from './footer/footer.component';
 import { SobreComponent } from './sobre/sobre.component';
@@ -21,14 +19,12 @@ import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { AuthService } from "./shared/services/auth.service";
+import { AuthService } from './shared/services/auth.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     HomeComponent,
-    RegistrarComponent,
     NavBarCliComponent,
     FooterComponent,
     SobreComponent,
